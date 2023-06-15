ed3118bc47e66e042ad640432a5ae2a6cbf5b867
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-06-15 16:04:46 +0300

    1 Done logic of Task 8

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 
 sizeA = 10
 arr = []
+
+for i in range(sizeA):
+    temp = []
+    if i == 0 or i == sizeA - 1:
+        for j in range(sizeA):
+            temp.append("*")
+    else:
+        for j in range(sizeA):
+            if j == 0 or j == sizeA - 1:
+                temp.append("*")
+            else:
+                temp.append("-")
+    arr.append(temp)
+
+print("\nmatrix size {} x {}".format(sizeA, sizeA))
+for i in arr:
+    for j in i:
+        print(j, end='  ')
+    print()
